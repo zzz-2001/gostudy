8e5b740946344128bda1edc6ca4cd2dd78dc4bd2
zzz_wgd <3224698814@qq.com>
2023-11-24 15:15:33 +0800

    dev:add some code

---
+++ go.mod
@@ -0,0 +1,15 @@
+module ginchat
+
+go 1.21.4
+
+require gorm.io/driver/mysql v1.5.2
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.18 // indirect
+	gorm.io/driver/sqlite v1.5.4 // indirect
+	gorm.io/gorm v1.25.5 // indirect
+)
---
+++ model/UserBasic.go
@@ -0,0 +1,23 @@
+package model
+
+import "gorm.io/gorm"
+
+type UserBasic struct {
+	gorm.Model
+	Name          string
+	PassWord      string
+	Phone         string
+	Email         string
+	Identity      string
+	ClientIp      string
+	ClientPort    string
+	LoginTime     uint64
+	HeartbeatTime uint64
+	LogOutTime    uint64
+	IsLogout      bool
+	DeviceInfo    string
+}
+
+func (table *UserBasic) TableName() string {
+	return "user_basic"
+}
