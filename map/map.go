48f5374ebaf07b098f238a5eab7f0a7c20c4bec2
zzz_wgd <3224698814@qq.com>
2023-11-25 12:46:25 +0800

    get some code

---
+++ map/map.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//输出按照首字母顺序排序
+	people := make(map[string]string)
+
+	people["name"] = "zck"
+	people["age"] = "18"
+	people["sex"] = "男"
+	people["live"] = "兰州"
+
+	fmt.Println(people)
+
+	fmt.Println(people["name"])
+
+	var dog = map[string]string{
+		"name":   "xzh",
+		"age":    "180",
+		"weight": "200",
+		"height": "165",
+	}
+
+	fmt.Println(dog)
+
+}
